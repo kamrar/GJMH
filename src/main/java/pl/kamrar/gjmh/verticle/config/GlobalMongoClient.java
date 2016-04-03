0f31bbe9b989ae7f2a2b05db2ac0d9df9e32ed74
Kamil Raróg <kamil.rarog@goeuro.com>
2016-04-03 18:09:34 +0200

    verticle init fix

--- src/main/java/pl/kamrar/gjmh/verticle/config/GlobalMongoClient.java
+++ src/main/java/pl/kamrar/gjmh/verticle/config/GlobalMongoClient.java
@@ -1,29 +1,31 @@
 package pl.kamrar.gjmh.verticle.config;
 
 import io.vertx.core.Future;
-import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.ext.mongo.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class GlobalMongoClient extends AbstractVerticle{
+import java.util.concurrent.CompletableFuture;
 
-    private static MongoClient mongoClient;
+@Configuration
+public class GlobalMongoClient extends AbstractVerticle {
+
+    private static CompletableFuture<MongoClient> mongoClient;
 
     @Override
     public void start(Future<Void> future) throws Exception {
-
         JsonObject config = new JsonObject()
                 .put("db_name", "gjmh")
                 .put("host", "localhost")
                 .put("port", 27017);
-
-        mongoClient = MongoClient.createShared(vertx, config);
+        mongoClient = CompletableFuture.supplyAsync(() -> MongoClient.createShared(vertx, config));
 
     }
 
     @Bean
-    MongoClient mongoClient(){ return mongoClient;}
+    MongoClient mongoClient() throws Exception {
+        return mongoClient.get();
+    }
 }
